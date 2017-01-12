525b1139fa82157452e40fb523f500c504f8aeb0
David Barkman <dbarkman@PHX-028324.local>
2017-01-12 00:38:45 -0500

    added unit test projects and 1 test

---
+++ EngineTests/PlayerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Tests
+{
+    [TestClass()]
+    public class PlayerTests
+    {
+        [TestMethod()]
+        public void PlayerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasRequiredItemToEnterLocationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasThisQuestTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CompletedThisQuestTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasAllQuestCompletionItemsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveQuestCompletionItemsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddItemToInventoryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveItemFromInventoryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MarkQuestCompletedTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ SuperAdventureTests/SuperAdventureTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperAdventure;
+using Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperAdventure.Tests
+{
+    [TestClass()]
+    public class SuperAdventureTests
+    {
+        [TestMethod()]
+        public void SuperAdventureTest()
+        {
+        }
+
+        [TestMethod()]
+        public void FullyHealPlayerTest()
+        {
+            SuperAdventure superAdventure = new SuperAdventure();
+            var privateObject = new PrivateObject(superAdventure);
+            Player player = new Player(1,10,20,0,1);
+            int expectedHitPoints = player.MaximumHitPoints;
+
+            privateObject.SetField("_player", player);
+            privateObject.Invoke("FullyHealPlayer");
+
+            Assert.AreEqual(player.CurrentHitPoints, expectedHitPoints);
+        }
+    }
+}
